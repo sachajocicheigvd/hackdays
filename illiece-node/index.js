9e04c507dc359ff2e888c38076bc2fa2ea9804e1
illiecemessaoudi <illiecemessah@gmail.com>
2023-09-06 14:54:54 +0200

    feat: articles citites json

--- illiece-node/index.js
+++ illiece-node/index.js
@@ -1,46 +1,42 @@
-const fs = require('fs')
-const readXlsxFile = require('read-excel-file/node')
+var xlsx = require('node-xlsx').default;
+var fs = require('fs')
 
 
 
 
 const geoAppJson = require('./newgeo.json');
+const articleAppJson = require('./source.json');
 
 
-function containsAny(str, substrings) {
-    for (var i = 0; i != substrings.length; i++) {
-       var substring = substrings[i];
-       if (str.indexOf(substring) != - 1) {
-         return substring;
-       }
-    }
-    return null; 
-}
+
+
+
 
 const geoJson = JSON.parse(JSON.stringify(geoAppJson))
+const articleJson = JSON.parse(JSON.stringify(articleAppJson))
 
-// File path.
-readXlsxFile('./pop.xlsx').then((rows) => {
-  const cleanedFeatures = [];
-  rows.map(row => {
-     geoJson.features.map((feature) => {
-      if (row[0] && row[0].includes(`......${feature.properties["gemeinde.BFS_NUMMER"]}`)) {
-        console.log('occurence', {...feature,  properties: { ...feature.properties, ...{population: row[1]}}})
-        cleanedFeatures.push({...feature,  properties: { ...feature.properties, ...{population: row[1]}}})
-      } else {
-        cleanedFeatures.push({...feature,  properties: { ...feature.properties, ...{population: null}}})
+console.log(articleJson);
+
+const articlesByCity = [];
+
+  geoJson.features.map((feature) => {
+    const articlesFilteredByCity = [];
+    articleJson.map((article) => {
+      if(article && article.city && article.city == feature.properties["gemeinde.NAME"]) {
+        articlesFilteredByCity.push(article);
       }
     })
+    articlesByCity.push({city: feature.properties["gemeinde.NAME"], articles: articlesFilteredByCity, count: articlesFilteredByCity.length});
   })
-  geoJson.features = cleanedFeatures;
 
-  // // converting the JSON object to a string
-});
 
-const data =  JSON.stringify(geoJson);
+ console.log(articlesByCity);
+
+
+const data =  JSON.stringify(articlesByCity);
 
 // writing the JSON string content to a file
-fs.writeFileSync("newGEO2.json", data, (error) => {
+fs.writeFileSync("articlesByCities.json", data, (error) => {
   // throwing the error
   // in case of a writing problem
   if (error) {
